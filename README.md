f32c4efd0ea6be599f241fb75f6ae5af9f886272
Thomas Geissl <thomas.geissl@gmail.com>
2015-07-24 14:08:07 +0200

    using ofFilePath::getUserHomeDir() now instead of ~, added basic windows support

---
+++ README.md
@@ -0,0 +1,15 @@
+ofxStandardPaths
+===
+
+Description
+---
+ofxStandardPaths is an addon for openframeworks <https://github.com/openframeworks/openframeworks>.
+It provides an easy way to access standard paths.
+
+This is still a work in progress. I only needed to access media directories so far. Locations such appData, appLocalData, appConfig still need to be discussed.
+
+Installation
+---
+```
+git clone https://github.com/thomasgeissl/ofxStandardPaths.git /path/to/your/openframeworks/addons/ofxStandardPaths
+```
--- src/ofxStandardPaths.h
+++ src/ofxStandardPaths.h
@@ -1,28 +1,28 @@
-#pragma once
-#include "ofConstants.h"
 //http://doc.qt.io/qt-5/qstandardpaths.html
-
+//https://github.com/pocoproject/poco/pull/561
+#pragma once
+#include <string>
 class ofxStandardPaths
 {
 public:
-    static const string DesktopLocation;
-    static const string DocumentsLocation;
-    static const string FontsLocation;
-    static const string ApplicationsLocation;
-    static const string MusicLocation;
-    static const string MovieLocation;
-    static const string PicturesLocation;
-    static const string TempLocation;
-    static const string HomeLocation;
-    static const string DataLocation;
-    static const string CacheLocation;
-    static const string GenericDataLocation;
-    static const string RuntimeDataLocation;
-    static const string ConfigLocation;
-    static const string GenericConfigLocation;
-    static const string DownloadLocation;
-    static const string GenericCacheLocation;
-    static const string AppDataLocation;
-    static const string AppLocalDataLocation;
-    static const string AppConfigLocation;
+    static const std::string DesktopLocation;
+    static const std::string DocumentsLocation;
+    static const std::string FontsLocation;
+    static const std::string ApplicationsLocation;
+    static const std::string MusicLocation;
+    static const std::string MoviesLocation;
+    static const std::string PicturesLocation;
+    static const std::string TempLocation;
+    static const std::string HomeLocation;
+    static const std::string DataLocation;
+    static const std::string CacheLocation;
+    static const std::string GenericDataLocation;
+    static const std::string RuntimeDataLocation;
+    static const std::string ConfigLocation;
+    static const std::string GenericConfigLocation;
+    static const std::string DownloadLocation;
+    static const std::string GenericCacheLocation;
+    static const std::string AppDataLocation;
+    static const std::string AppLocalDataLocation;
+    static const std::string AppConfigLocation;
 };
